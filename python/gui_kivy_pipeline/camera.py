0ae9c39daf7dcf99e6162fc69fd3218004ea85b1
Jimbopython <le.stoffers@gmail.com>
2026-06-22 07:57:58 +0200

    feat: improve in-code documentation

--- python/gui_kivy_pipeline/camera.py
+++ python/gui_kivy_pipeline/camera.py
@@ -39,7 +39,7 @@ class Camera:
         instance = ids_peak.DeviceManager.Instance()
         instance.Update()
 
-        # Filter out devices which cannot be opened
+        # Create a list with all openable devices.
         devices = list(
             filter(
                 lambda dev: dev.IsOpenable(ids_peak.DeviceAccessType_Control),
